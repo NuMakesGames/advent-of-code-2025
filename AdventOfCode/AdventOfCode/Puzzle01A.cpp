2e1203811a3e5460e27964a28e2bd55c43c48f02
Marcus Reid <NuMakesGames@users.noreply.github.com>
2025-12-01 05:53:30 +0000

    Solve day 1.

--- AdventOfCode/AdventOfCode/Puzzle01A.cpp
+++ AdventOfCode/AdventOfCode/Puzzle01A.cpp
@@ -10,9 +10,49 @@ using namespace nu::console::vt;
 
 namespace Puzzle01A
 {
+	// 1147
 	std::string Solve(const std::vector<std::string>& inputLines)
 	{
-		return "Puzzle01A not yet solved!";
+		int dial = 50;
+		int numZeros = 0;
+		for (const auto& line : inputLines)
+		{
+			// Extract rotations
+			int totalRotation = 0;
+			for (int i = 1; i < line.size(); ++i)
+			{
+				totalRotation *= 10;
+				totalRotation += line[i] - '0';
+			}
+
+			// Turn the dial left or right based on instruction
+			constexpr int numDialValues = 100;
+			int partialRotation = totalRotation % numDialValues;
+			if (line[0] == 'L')
+			{
+				dial -= partialRotation;
+				if (dial < 0)
+				{
+					dial += numDialValues;
+				}
+			}
+			else
+			{
+				dial += partialRotation;
+				if (dial > numDialValues - 1)
+				{
+					dial -= numDialValues;
+				}
+			}
+
+			// Count if the dial ended on zero
+			if (dial == 0)
+			{
+				++numZeros;
+			}
+		}
+
+		return std::to_string(numZeros);
 	}
 
 	std::string Visualize(const std::vector<std::string>& inputLines)
--- AdventOfCode/AdventOfCode/Puzzle01B.cpp
+++ AdventOfCode/AdventOfCode/Puzzle01B.cpp
@@ -10,9 +10,65 @@ using namespace nu::console::vt;
 
 namespace Puzzle01B
 {
+	// 6789
 	std::string Solve(const std::vector<std::string>& inputLines)
 	{
-		return "Puzzle01B not yet solved!";
+		int dial = 50;
+		int numZeros = 0;
+		for (const auto& line : inputLines)
+		{
+			// Extract rotations
+			int totalRotation = 0;
+			for (int i = 1; i < line.size(); ++i)
+			{
+				totalRotation *= 10;
+				totalRotation += line[i] - '0';
+			}
+
+			// Full rotations always cross zero
+			constexpr int numDialValues = 100;
+			auto fullRotations = totalRotation / numDialValues;
+			numZeros += fullRotations;
+
+			// Turn the dial left or right based on instruction
+			auto remainingRotation = totalRotation % numDialValues;
+			if (line[0] == 'L')
+			{
+				if (dial == 0)
+				{
+					// Left turn started from zero; don't double count this zero when wrapping
+					dial -= remainingRotation;
+					dial += numDialValues;
+				}
+				else
+				{
+					dial -= remainingRotation;
+					if (dial == 0)
+					{
+						// Left turn stopped on zero
+						++numZeros;
+					}
+					else if (dial < 0)
+					{
+						// Left turn crossed zero
+						++numZeros;
+						dial += numDialValues;
+					}
+				}
+			}
+			else
+			{
+				dial += remainingRotation;
+				if (dial > numDialValues - 1)
+				{
+					// Right turn crossed zero
+					dial -= numDialValues;
+					++numZeros;
+				}
+			}
+		}
+
+		return std::to_string(numZeros);
 	}
 
 	std::string Visualize(const std::vector<std::string>& inputLines)
