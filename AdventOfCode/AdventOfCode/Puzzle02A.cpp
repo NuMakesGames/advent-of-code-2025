4d45130b1966424ffcae030c18bc865611e81409
Marcus Reid <NuMakesGames@users.noreply.github.com>
2025-12-02 22:19:57 +0000

    Parallelize day 2 for an order of magnitude speedup.

--- AdventOfCode/AdventOfCode/Puzzle02A.cpp
+++ AdventOfCode/AdventOfCode/Puzzle02A.cpp
@@ -10,37 +10,56 @@ using namespace nu::console::vt;
 
 namespace Puzzle02A
 {
+	std::unordered_set<uint64_t> FindInvalidIds(uint64_t firstId, uint64_t lastId)
+	{
+		std::unordered_set<uint64_t> invalidIds;
+		for (auto id = firstId; id <= lastId; ++id)
+		{
+			std::string idStr = std::to_string(id);
+			if (idStr.size() % 2 != 0)
+			{
+				continue;
+			}
+
+			bool isInvalid = true;
+			for (auto i = 0; i < idStr.size() / 2; ++i)
+			{
+				if (idStr[i] != idStr[idStr.size() / 2 + i])
+				{
+					isInvalid = false;
+					break;
+				}
+			}
+
+			if (isInvalid)
+			{
+				invalidIds.insert(id);
+			}
+		}
+
+		return invalidIds;
+	}
+
 	// 8576933996
 	std::string Solve(const std::vector<std::string>& inputLines)
 	{
 		auto productRanges = Utilities::SplitString(inputLines[0], ",");
-		std::unordered_set<uint64_t> invalidIds;
+		auto futures = std::vector<std::future<std::unordered_set<uint64_t>>>{};
+		futures.reserve(productRanges.size());
+
 		for (const auto& productRange : productRanges)
 		{
 			auto bounds = Utilities::SplitString(productRange, "-");
-			for (auto id = std::stoull(bounds[0]); id <= std::stoull(bounds[1]); ++id)
-			{
-				std::string idStr = std::to_string(id);
-				if (idStr.size() % 2 != 0)
-				{
-					continue;
-				}
+			auto firstId = std::stoull(bounds[0]);
+			auto lastId = std::stoull(bounds[1]);
+			futures.push_back(std::async(std::launch::async, FindInvalidIds, firstId, lastId));
+		}
 
-				bool isInvalid = true;
-				for (auto i = 0; i < idStr.size() / 2; ++i)
-				{
-					if (idStr[i] != idStr[idStr.size() / 2 + i])
-					{
-						isInvalid = false;
-						break;
-					}
-				}
-
-				if (isInvalid)
-				{
-					invalidIds.insert(id);
-				}
-			}
+		std::unordered_set<uint64_t> invalidIds;
+		for (auto& future : futures)
+		{
+			auto result = future.get();
+			invalidIds.insert_range(result);
 		}
 
 		return std::to_string(std::accumulate(invalidIds.begin(), invalidIds.end(), 0ull));
--- AdventOfCode/AdventOfCode/Puzzle02B.cpp
+++ AdventOfCode/AdventOfCode/Puzzle02B.cpp
@@ -10,46 +10,65 @@ using namespace nu::console::vt;
 
 namespace Puzzle02B
 {
+	std::unordered_set<uint64_t> FindInvalidIds(uint64_t firstId, uint64_t lastId)
+	{
+		std::unordered_set<uint64_t> invalidIds;
+		for (auto id = firstId; id <= lastId; ++id)
+		{
+			std::string idStr = std::to_string(id);
+			for (auto repeats = 2; repeats <= idStr.size(); ++repeats)
+			{
+				bool isInvalid = true;
+				if (idStr.size() % repeats != 0)
+				{
+					continue;
+				}
+
+				auto stride = idStr.size() / repeats;
+				for (auto i = 0; i < stride; ++i)
+				{
+					auto next = i + stride;
+					while (next < idStr.size())
+					{
+						if (idStr[i] != idStr[next])
+						{
+							isInvalid = false;
+							break;
+						}
+						next += stride;
+					}
+				}
+
+				if (isInvalid)
+				{
+					invalidIds.insert(id);
+				}
+			}
+		}
+
+		return invalidIds;
+	}
+
 	// 25663320831
 	std::string Solve(const std::vector<std::string>& inputLines)
 	{
 		auto productRanges = Utilities::SplitString(inputLines[0], ",");
-		std::unordered_set<uint64_t> invalidIds;
+		auto futures = std::vector<std::future<std::unordered_set<uint64_t>>>{};
+		futures.reserve(productRanges.size());
+
 		for (const auto& productRange : productRanges)
 		{
 			auto bounds = Utilities::SplitString(productRange, "-");
-			for (auto id = std::stoull(bounds[0]); id <= std::stoull(bounds[1]); ++id)
-			{
-				std::string str = std::to_string(id);
-				for (auto repeats = 2; repeats <= str.size(); ++repeats)
-				{
-					bool isInvalid = true;
-					if (str.size() % repeats != 0)
-					{
-						continue;
-					}
+			auto firstId = std::stoull(bounds[0]);
+			auto lastId = std::stoull(bounds[1]);
+			futures.push_back(std::async(std::launch::async, FindInvalidIds, firstId, lastId));
+		}
 
-					auto stride = str.size() / repeats;
-					for (auto i = 0; i < stride; ++i)
-					{
-						auto next = i + stride;
-						while (next < str.size())
-						{
-							if (str[i] != str[next])
-							{
-								isInvalid = false;
-								break;
-							}
-							next += stride;
-						}
-					}
-
-					if (isInvalid)
-					{
-						invalidIds.insert(id);
-					}
-				}
-			}
+		std::unordered_set<uint64_t> invalidIds;
+		for (auto& future : futures)
+		{
+			auto result = future.get();
+			invalidIds.insert_range(result);
 		}
 
 		return std::to_string(std::accumulate(invalidIds.begin(), invalidIds.end(), 0ull));
