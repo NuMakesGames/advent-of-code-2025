b40c6f9e2e6064a598d9eead91e389376d1588f7
Marcus Reid <NuMakesGames@users.noreply.github.com>
2025-12-02 05:41:22 +0000

    Solve day 2.

--- AdventOfCode/AdventOfCode/Puzzle02A.cpp
+++ AdventOfCode/AdventOfCode/Puzzle02A.cpp
@@ -10,9 +10,40 @@ using namespace nu::console::vt;
 
 namespace Puzzle02A
 {
+	// 8576933996
 	std::string Solve(const std::vector<std::string>& inputLines)
 	{
-		return "Puzzle02A not yet solved!";
+		auto productRanges = Utilities::SplitString(inputLines[0], ",");
+		std::unordered_set<uint64_t> invalidIds;
+		for (const auto& productRange : productRanges)
+		{
+			auto bounds = Utilities::SplitString(productRange, "-");
+			for (auto id = std::stoull(bounds[0]); id <= std::stoull(bounds[1]); ++id)
+			{
+				std::string idStr = std::to_string(id);
+				if (idStr.size() % 2 != 0)
+				{
+					continue;
+				}
+
+				bool isInvalid = true;
+				for (auto i = 0; i < idStr.size() / 2; ++i)
+				{
+					if (idStr[i] != idStr[idStr.size() / 2 + i])
+					{
+						isInvalid = false;
+						break;
+					}
+				}
+
+				if (isInvalid)
+				{
+					invalidIds.insert(id);
+				}
+			}
+		}
+
+		return std::to_string(std::accumulate(invalidIds.begin(), invalidIds.end(), 0ull));
 	}
 
 	std::string Visualize(const std::vector<std::string>& inputLines)
--- AdventOfCode/AdventOfCode/Puzzle02B.cpp
+++ AdventOfCode/AdventOfCode/Puzzle02B.cpp
@@ -10,9 +10,49 @@ using namespace nu::console::vt;
 
 namespace Puzzle02B
 {
+	// 25663320831
 	std::string Solve(const std::vector<std::string>& inputLines)
 	{
-		return "Puzzle02B not yet solved!";
+		auto productRanges = Utilities::SplitString(inputLines[0], ",");
+		std::unordered_set<uint64_t> invalidIds;
+		for (const auto& productRange : productRanges)
+		{
+			auto bounds = Utilities::SplitString(productRange, "-");
+			for (auto id = std::stoull(bounds[0]); id <= std::stoull(bounds[1]); ++id)
+			{
+				std::string str = std::to_string(id);
+				for (auto repeats = 2; repeats <= str.size(); ++repeats)
+				{
+					bool isInvalid = true;
+					if (str.size() % repeats != 0)
+					{
+						continue;
+					}
+
+					auto stride = str.size() / repeats;
+					for (auto i = 0; i < stride; ++i)
+					{
+						auto next = i + stride;
+						while (next < str.size())
+						{
+							if (str[i] != str[next])
+							{
+								isInvalid = false;
+								break;
+							}
+							next += stride;
+						}
+					}
+
+					if (isInvalid)
+					{
+						invalidIds.insert(id);
+					}
+				}
+			}
+		}
+
+		return std::to_string(std::accumulate(invalidIds.begin(), invalidIds.end(), 0ull));
 	}
 
 	std::string Visualize(const std::vector<std::string>& inputLines)
