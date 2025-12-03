b6a1a5c84034ca2a176d97f9605701ca7f19de42
Marcus Reid <NuMakesGames@users.noreply.github.com>
2025-12-03 04:57:20 +0000

    Make day 2, part 1 fast.

--- AdventOfCode/AdventOfCode/Puzzle02A.cpp
+++ AdventOfCode/AdventOfCode/Puzzle02A.cpp
@@ -10,59 +10,82 @@ using namespace nu::console::vt;
 
 namespace Puzzle02A
 {
-	std::unordered_set<uint64_t> FindInvalidIds(uint64_t firstId, uint64_t lastId)
-	{
-		std::unordered_set<uint64_t> invalidIds;
-		for (auto id = firstId; id <= lastId; ++id)
-		{
-			std::string idStr = std::to_string(id);
-			if (idStr.size() % 2 != 0)
-			{
-				continue;
-			}
+	constexpr std::array<uint64_t, 14> powers = {
+		1,          10,          100,           1'000,          10'000,          100'000,           1'000'000,
+		10'000'000, 100'000'000, 1'000'000'000, 10'000'000'000, 100'000'000'000, 1'000'000'000'000, 10'000'000'000'000
+	};
 
-			bool isInvalid = true;
+	std::tuple<uint64_t, uint64_t, uint64_t> FindNextInvalidId(const std::string& idStr)
+	{
+		auto id = std::stoull(idStr);
+		auto pattern = 0ull;
+		auto minDigits = idStr.size();
+		if (minDigits % 2 != 0)
+		{
+			minDigits = minDigits + 1;
+			pattern = powers[minDigits / 2 - 1];
+		}
+		else
+		{
 			for (auto i = 0; i < idStr.size() / 2; ++i)
 			{
-				if (idStr[i] != idStr[idStr.size() / 2 + i])
-				{
-					isInvalid = false;
-					break;
-				}
-			}
-
-			if (isInvalid)
-			{
-				invalidIds.insert(id);
+				pattern = pattern * 10 + (idStr[i] - '0');
 			}
 		}
 
-		return invalidIds;
+		auto invalidId = pattern * powers[minDigits / 2] + pattern;
+		while (invalidId < id)
+		{
+			pattern++;
+			invalidId = pattern * powers[minDigits / 2] + pattern;
+		}
+
+		return std::make_tuple(invalidId, pattern, minDigits);
+	}
+
+	std::pair<uint64_t, uint64_t> FindPreviousInvalidId(const std::string& idStr)
+	{
+		auto id = std::stoull(idStr);
+		auto maxDigits = idStr.size();
+		if (maxDigits % 2 != 0)
+		{
+			maxDigits = maxDigits + - 1;
+		}
+	
+		auto pattern = powers[maxDigits / 2] - 1;
+		auto invalidId = pattern * powers[maxDigits / 2] + pattern;
+		while (invalidId > id)
+		{
+			pattern--;
+			invalidId = pattern * powers[maxDigits / 2] + pattern;
+		}
+
+		return std::make_pair(invalidId, pattern);
 	}
 
 	// 8576933996
 	std::string Solve(const std::vector<std::string>& inputLines)
 	{
 		auto productRanges = Utilities::SplitString(inputLines[0], ",");
-		auto futures = std::vector<std::future<std::unordered_set<uint64_t>>>{};
-		futures.reserve(productRanges.size());
-
+		auto total = 0ull;
 		for (const auto& productRange : productRanges)
 		{
 			auto bounds = Utilities::SplitString(productRange, "-");
-			auto firstId = std::stoull(bounds[0]);
-			auto lastId = std::stoull(bounds[1]);
-			futures.push_back(std::async(std::launch::async, FindInvalidIds, firstId, lastId));
+			auto firstId = bounds[0];
+			auto lastId = bounds[1];
+			auto [firstInvalidId, firstPattern, minDigits] = FindNextInvalidId(firstId);
+			auto [lastInvalidId, lastPattern] = FindPreviousInvalidId(lastId);
+			if (firstInvalidId > lastInvalidId)
+			{
+				continue;
+			}
+
+			auto sum = lastPattern * (lastPattern + 1) / 2 - (firstPattern - 1) * firstPattern / 2;
+			auto multiplier = powers[minDigits / 2] + 1; 
+			total += sum * multiplier;
 		}
 
-		std::unordered_set<uint64_t> invalidIds;
-		for (auto& future : futures)
-		{
-			auto result = future.get();
-			invalidIds.insert_range(result);
-		}
-
-		return std::to_string(std::accumulate(invalidIds.begin(), invalidIds.end(), 0ull));
+		return std::to_string(total);
 	}
 
 	std::string Visualize(const std::vector<std::string>& inputLines)
