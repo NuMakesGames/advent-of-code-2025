cf831b7a5fac62976ea4764faadfee61c312c2f0
Marcus Reid <NuMakesGames@users.noreply.github.com>
2025-12-03 05:36:36 +0000

    Solve day 3.

--- AdventOfCode/AdventOfCode/Puzzle03A.cpp
+++ AdventOfCode/AdventOfCode/Puzzle03A.cpp
@@ -10,9 +10,51 @@ using namespace nu::console::vt;
 
 namespace Puzzle03A
 {
+	// 17113
 	std::string Solve(const std::vector<std::string>& inputLines)
 	{
-		return "Puzzle03A not yet solved!";
+		auto result = 0ull;
+
+		auto banks = std::vector<std::vector<int32_t>>{};
+		for (const auto& line : inputLines)
+		{
+			std::vector<int> bank;
+			for (char c : line)
+			{
+				bank.emplace_back(c - '0');
+			}
+			banks.emplace_back(bank);
+		}
+
+		int jolts = 0;
+		for (const auto& bank : banks)
+		{
+			int firstIndex = 0;
+			int firstBattery = bank[firstIndex];
+			for (int i = 1; i < bank.size() - 1; ++i)
+			{
+				if (bank[i] > firstBattery)
+				{
+					firstIndex = i;
+					firstBattery = bank[i];
+				}
+			}
+
+			int secondIndex = firstIndex + 1;
+			int secondBattery = bank[secondIndex];
+			for (int j = secondIndex; j < bank.size(); ++j)
+			{
+				if (bank[j] > secondBattery)
+				{
+					secondIndex = j;
+					secondBattery = bank[j];
+				}
+			}
+
+			jolts += firstBattery * 10 + secondBattery;
+		}
+
+		return std::to_string(jolts);
 	}
 
 	std::string Visualize(const std::vector<std::string>& inputLines)
--- AdventOfCode/AdventOfCode/Puzzle03B.cpp
+++ AdventOfCode/AdventOfCode/Puzzle03B.cpp
@@ -10,9 +10,61 @@ using namespace nu::console::vt;
 
 namespace Puzzle03B
 {
+	// 169709990062889
 	std::string Solve(const std::vector<std::string>& inputLines)
 	{
-		return "Puzzle03B not yet solved!";
+		auto banks = std::vector<std::vector<int32_t>>{};
+		for (const auto& line : inputLines)
+		{
+			std::vector<int> bank;
+			for (char c : line)
+			{
+				bank.emplace_back(c - '0');
+			}
+			banks.emplace_back(bank);
+		}
+
+		int64_t totalJolts = 0;
+		for (const auto& bank : banks)
+		{
+			constexpr int requiredBatteries = 12;
+			std::vector<int> batteries;
+			batteries.reserve(requiredBatteries);
+
+			size_t minIndex = 0;
+			while (batteries.size() < requiredBatteries)
+			{
+				for (int optimalBattery = 9; optimalBattery >= 1; --optimalBattery)
+				{
+					bool foundBattery = false;
+					for (size_t i = minIndex; i <= bank.size() - (requiredBatteries - batteries.size()); ++i)
+					{
+						if (bank[i] == optimalBattery)
+						{
+							batteries.emplace_back(bank[i]);
+							minIndex = i + 1;
+							foundBattery = true;
+							break;
+						}
+					}
+
+					if (foundBattery)
+					{
+						break;
+					}
+				}
+			}
+
+			int64_t jolts = 0;
+			for (auto battery : batteries)
+			{
+				jolts = jolts * 10 + battery;
+			}
+
+			totalJolts += jolts;
+		}
+
+		return std::to_string(totalJolts);
 	}
 
 	std::string Visualize(const std::vector<std::string>& inputLines)
