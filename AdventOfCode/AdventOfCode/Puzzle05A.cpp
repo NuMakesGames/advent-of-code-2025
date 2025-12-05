2c17cd84648c2cb702863b824eb4beb8528cc6be
Marcus Reid <NuMakesGames@users.noreply.github.com>
2025-12-05 06:15:47 +0000

    Solve day 5.

--- AdventOfCode/AdventOfCode/Puzzle05A.cpp
+++ AdventOfCode/AdventOfCode/Puzzle05A.cpp
@@ -10,9 +10,53 @@ using namespace nu::console::vt;
 
 namespace Puzzle05A
 {
+	struct IngredientRange
+	{
+		uint64_t start = 0;
+		uint64_t end = 0;
+
+		bool IsIngredientFresh(uint64_t v) const
+		{
+			return start <= v && v <= end;
+		}
+	};
+
+	// 525
 	std::string Solve(const std::vector<std::string>& inputLines)
 	{
-		return "Puzzle05A not yet solved!";
+		// Parse fresh ingredient ranges
+		std::vector<IngredientRange> ingredientRanges;
+		int i = 0;
+		for (; i < inputLines.size(); ++i)
+		{
+			const auto& line = inputLines[i];
+			if (line.empty())
+			{
+				++i;
+				break;
+			}
+
+			auto ingredients = Utilities::SplitString(line, "-");
+			ingredientRanges.emplace_back(std::stoull(ingredients[0]), std::stoull(ingredients[1]));
+		}
+
+		// Parse ingredients to check
+		std::vector<uint64_t> ingredients;
+		for (; i < inputLines.size(); ++i)
+		{
+			ingredients.emplace_back(std::stoull(inputLines[i]));
+		}
+
+		// Check for freshness
+		auto freshIngredientCount = std::ranges::count_if(
+			ingredients,
+			[&ingredientRanges](uint64_t ingredient)
+			{
+				return std::ranges::any_of(
+					ingredientRanges, [ingredient](const IngredientRange& r) { return r.IsIngredientFresh(ingredient); });
+			});
+
+		return std::to_string(freshIngredientCount);
 	}
 
 	std::string Visualize(const std::vector<std::string>& inputLines)
--- AdventOfCode/AdventOfCode/Puzzle05B.cpp
+++ AdventOfCode/AdventOfCode/Puzzle05B.cpp
@@ -10,9 +10,58 @@ using namespace nu::console::vt;
 
 namespace Puzzle05B
 {
+	struct IngredientRange
+	{
+		uint64_t start = 0;
+		uint64_t end = 0;
+
+		auto operator<=>(const IngredientRange&) const = default;
+	};
+
+	// 333892124923577
 	std::string Solve(const std::vector<std::string>& inputLines)
 	{
-		return "Puzzle05B not yet solved!";
+		// Parse fresh ingredient ranges
+		std::vector<IngredientRange> ingredientRanges;
+		for (auto i = 0; i < inputLines.size(); ++i)
+		{
+			const auto& line = inputLines[i];
+			if (line.empty())
+			{
+				break;
+			}
+
+			auto ingredients = Utilities::SplitString(line, "-");
+			ingredientRanges.emplace_back(std::stoull(ingredients[0]), std::stoull(ingredients[1]));
+		}
+
+		// Merge all ranges in-place
+		std::ranges::sort(ingredientRanges);
+		auto iCurrent = 0;
+		for (auto iNext = 1; iNext < ingredientRanges.size(); ++iNext)
+		{
+			IngredientRange& current = ingredientRanges[iCurrent];
+			const IngredientRange& next = ingredientRanges[iNext];
+
+			if (next.start <= current.end)
+			{
+				current.end = std::max(current.end, next.end);
+			}
+			else
+			{
+				ingredientRanges[++iCurrent] = next;
+			}
+		}
+		ingredientRanges.resize(iCurrent + 1);
+
+		// Count the total fresh ingredients
+		auto totalFreshIngredients = std::accumulate(
+			ingredientRanges.cbegin(),
+			ingredientRanges.cend(),
+			0ull,
+			[](uint64_t acc, const IngredientRange& r) { return acc + r.end - r.start + 1; });
+
+		return std::to_string(totalFreshIngredients);
 	}
 
 	std::string Visualize(const std::vector<std::string>& inputLines)
