66abef621f6ebc120e702ce43d81528383601c55
Marcus Reid <NuMakesGames@users.noreply.github.com>
2025-12-07 05:45:57 +0000

    Solve day 7.

--- AdventOfCode/AdventOfCode/Puzzle07A.cpp
+++ AdventOfCode/AdventOfCode/Puzzle07A.cpp
@@ -12,7 +12,63 @@ namespace Puzzle07A
 {
 	std::string Solve(const std::vector<std::string>& inputLines)
 	{
-		return "Puzzle07A not yet solved!";
+		auto grid = ConvertLinesToGrid(inputLines);
+		auto beamIndices = std::vector<int>{};
+		for (int i = 0; i < inputLines[0].size(); ++i)
+		{
+			if (inputLines[0][i] == 'S')
+			{
+				beamIndices.emplace_back(i);
+				break;
+			}
+		}
+
+		auto totalSplits = 0ull;
+		for (auto y = 0; y < grid.Height(); ++y)
+		{
+			auto nextBeamIndices = std::vector<int>{};
+			for (auto x : beamIndices)
+			{
+				constexpr auto down = Vector2d<int>{ 0, 1 };
+				auto next = Vector2d<int>{ x, y } + down;
+				if (!grid.IsInBounds(next))
+				{
+					continue;
+				}
+
+				auto c = grid.at(next);
+				switch (c)
+				{
+					case '.':
+						grid.at(next) = '|';
+						nextBeamIndices.emplace_back(next.x);
+						break;
+					case '^':
+						constexpr auto left = Vector2d<int>{ -1, 0 };
+						auto nextLeft = next + left;
+						if (grid.IsInBounds(nextLeft))
+						{
+							grid.at(nextLeft) = '|';
+							nextBeamIndices.emplace_back(nextLeft.x);
+						}
+
+						constexpr auto right = Vector2d<int>{ 1, 0 };
+						auto nextRight = next + right;
+						if (grid.IsInBounds(nextRight))
+						{
+							grid.at(nextRight) = '|';
+							nextBeamIndices.emplace_back(nextRight.x);
+						}
+
+						++totalSplits;
+						break;
+				}
+			}
+
+			beamIndices.swap(nextBeamIndices);
+		}
+
+		return std::to_string(totalSplits);
 	}
 
 	std::string Visualize(const std::vector<std::string>& inputLines)
--- AdventOfCode/AdventOfCode/Puzzle07B.cpp
+++ AdventOfCode/AdventOfCode/Puzzle07B.cpp
@@ -10,9 +10,69 @@ using namespace nu::console::vt;
 
 namespace Puzzle07B
 {
+	uint64_t Recurse(const auto& grid, const Vector2d<int>& currentBeam)
+	{
+		static auto memoize = std::unordered_map<Vector2d<int>, uint64_t>{};
+		if (memoize.contains(currentBeam))
+		{
+			return memoize[currentBeam];
+		}
+
+		if (currentBeam.y + 1 == grid.Height())
+		{
+			memoize[currentBeam] = 1;
+			return 1;
+		}
+
+		auto result = 0ull;
+		constexpr auto down = Vector2d<int>{ 0, 1 };
+		auto next = currentBeam + down;
+		auto c = grid.at(next);
+		switch (c)
+		{
+			case '.':
+				result = Recurse(grid, next);
+				break;
+			case '^':
+				constexpr auto left = Vector2d<int>{ -1, 0 };
+				constexpr auto right = Vector2d<int>{ 1, 0 };
+				auto nextLeft = next + left;
+				auto nextRight = next + right;
+				if (grid.IsInBounds(nextLeft) && grid.IsInBounds(nextRight))
+				{
+					result = Recurse(grid, nextLeft) + Recurse(grid, nextRight);
+				}
+				else if (grid.IsInBounds(nextLeft))
+				{
+					result = Recurse(grid, nextLeft);
+				}
+				else if (grid.IsInBounds(nextRight))
+				{
+					result = Recurse(grid, nextRight);
+				}
+				break;
+		}
+
+		memoize[currentBeam] = result;
+		return result;
+	}
+
+	// 24292631346665
 	std::string Solve(const std::vector<std::string>& inputLines)
 	{
-		return "Puzzle07B not yet solved!";
+		auto grid = ConvertLinesToGrid(inputLines);
+		auto startX = 0;
+		for (int i = 0; i < inputLines[0].size(); ++i)
+		{
+			if (inputLines[0][i] == 'S')
+			{
+				startX = i;
+				break;
+			}
+		}
+
+		auto result = Recurse(grid, Vector2d<int>{ startX, 0 });
+		return std::to_string(result);
 	}
 
 	std::string Visualize(const std::vector<std::string>& inputLines)
