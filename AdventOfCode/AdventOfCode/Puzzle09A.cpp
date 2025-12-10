86aea0c9e97a4ecefab93cae4f034725cc19aea6
Marcus Reid <>
2025-12-09 16:04:38 -0800

    Solve day 9, but it's slow.

--- AdventOfCode/AdventOfCode/Puzzle09A.cpp
+++ AdventOfCode/AdventOfCode/Puzzle09A.cpp
@@ -10,9 +10,34 @@ using namespace nu::console::vt;
 
 namespace Puzzle09A
 {
+	// 4767418746
 	std::string Solve(const std::vector<std::string>& inputLines)
 	{
-		return "Puzzle09A not yet solved!";
+		auto points = std::vector<Vector2d<int64_t>>{};
+		for (const auto& line : inputLines)
+		{
+			auto coords = ExtractInts<std::int64_t>(line);
+			points.emplace_back(coords[0], coords[1]);
+		}
+
+		auto maxArea = 0ll;
+		auto maxPoints = std::pair<Vector2d<int64_t>, Vector2d<int64_t>>{};
+		for (auto i = 0; i < points.size() - 1; ++i)
+		{
+			for (auto j = i + 1; j < points.size(); ++j)
+			{
+				auto dx = std::abs(points[i].x - points[j].x) + 1;
+				auto dy = std::abs(points[i].y - points[j].y) + 1;
+				auto area = dx * dy;
+				if (area > maxArea)
+				{
+					maxArea = area;
+					maxPoints = std::make_pair(points[i], points[j]);
+				}
+			}
+		}
+
+		return std::to_string(maxArea);
 	}
 
 	std::string Visualize(const std::vector<std::string>& inputLines)
--- AdventOfCode/AdventOfCode/Puzzle09B.cpp
+++ AdventOfCode/AdventOfCode/Puzzle09B.cpp
@@ -10,9 +10,102 @@ using namespace nu::console::vt;
 
 namespace Puzzle09B
 {
+	// 1461987144
 	std::string Solve(const std::vector<std::string>& inputLines)
 	{
-		return "Puzzle09B not yet solved!";
+		auto points = std::vector<Vector2d<int>>{};
+		for (const auto& line : inputLines)
+		{
+			auto coords = ExtractInts<int>(line);
+			points.emplace_back(coords[0], coords[1]);
+		}
+
+		// Build perimeter...
+		std::unordered_set<Vector2d<int>> perimeter;
+		for (auto i = 0; i < points.size(); ++i)
+		{
+			const auto& first = points[i];
+			const auto& second = points[i == points.size() - 1 ? 0 : i + 1];
+
+			if (first.x == second.x)
+			{
+				auto start = std::min(first.y, second.y);
+				auto end = std::max(first.y, second.y);
+				for (auto y = start; y <= end; ++y)
+				{
+					perimeter.insert(Vector2d<int>{ first.x, y });
+				}
+			}
+			else
+			{
+				auto start = std::min(first.x, second.x);
+				auto end = std::max(first.x, second.x);
+				for (auto x = start; x <= end; ++x)
+				{
+					perimeter.insert(Vector2d<int>{ x, first.y });
+				}
+			}
+		}
+
+		// Compute all rectangles
+		auto allRectangles = std::vector<std::tuple<uint64_t, Vector2d<int>, Vector2d<int>>>{};
+		for (auto i = 0; i < points.size() - 1; ++i)
+		{
+			for (auto j = i + 1; j < points.size(); ++j)
+			{
+				uint64_t dx = std::abs(points[i].x - points[j].x) + 1;
+				uint64_t dy = std::abs(points[i].y - points[j].y) + 1;
+				uint64_t area = dx * dy;
+				allRectangles.emplace_back(area, points[i], points[j]);
+			}
+		}
+		std::ranges::sort(allRectangles, std::greater<std::tuple<uint64_t, Vector2d<int>, Vector2d<int>>>{});
+
+		// Find largest valid rectangle by tracing interior
+		auto maxArea = 0ll;
+		auto maxPoints = std::pair<Vector2d<int>, Vector2d<int>>{};
+		for (const auto& [area, firstPoint, secondPoint] : allRectangles)
+		{
+			auto minX = std::min(firstPoint.x, secondPoint.x) + 1;
+			auto maxX = std::max(firstPoint.x, secondPoint.x) - 1;
+			auto minY = std::min(firstPoint.y, secondPoint.y) + 1;
+			auto maxY = std::max(firstPoint.y, secondPoint.y) - 1;
+
+			auto isValid = true;
+			for (auto x = minX; x <= maxX; ++x)
+			{
+				if (perimeter.contains(Vector2d<int>{ x, minY }) || perimeter.contains(Vector2d<int>{ x, maxY }))
+				{
+					isValid = false;
+					break;
+				}
+			}
+
+			if (!isValid)
+			{
+				continue;
+			}
+
+			for (auto y = minY; y <= maxY; ++y)
+			{
+				if (perimeter.contains(Vector2d<int>{ minX, y }) || perimeter.contains(Vector2d<int>{ maxX, y }))
+				{
+					isValid = false;
+					break;
+				}
+			}
+
+			if (!isValid)
+			{
+				continue;
+			}
+
+			maxArea = area;
+			maxPoints = std::make_pair(firstPoint, secondPoint);
+			break;
+		}
+
+		return std::to_string(maxArea);
 	}
 
 	std::string Visualize(const std::vector<std::string>& inputLines)
