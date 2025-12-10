c8a6cbdf63262a24f4ca9569acd8fe083ab2ce56
Marcus Reid <NuMakesGames@users.noreply.github.com>
2025-12-10 06:09:10 +0000

    Solve day 10. Part 2 solved using Z3 in python.

--- AdventOfCode/AdventOfCode/Puzzle10A.cpp
+++ AdventOfCode/AdventOfCode/Puzzle10A.cpp
@@ -10,9 +10,98 @@ using namespace nu::console::vt;
 
 namespace Puzzle10A
 {
+	struct State
+	{
+		std::bitset<10> lights = 0;
+		size_t nextButton = 0;
+
+		auto operator<=>(const State&) const = default;
+	};
+} // namespace Puzzle10A
+
+IMPLEMENT_STD_HASH(Puzzle10A::State, value.lights, value.nextButton);
+
+namespace Puzzle10A
+{
+	// 425
+	uint64_t SolveMachine(const std::bitset<10>& desiredLights, const std::vector<std::vector<int>>& buttons)
+	{
+		auto queue = std::queue<State>{};
+		auto seen = std::unordered_map<State, uint64_t>{};
+		for (auto i = 0; i < buttons.size(); ++i)
+		{
+			auto nextState = State{};
+			nextState.nextButton = i;
+			seen[nextState] = 0;
+			queue.push(std::move(nextState));
+		}
+
+		auto currentState = State{};
+		auto presses = 0ull;
+		while (currentState.lights != desiredLights)
+		{
+			auto previousState = queue.front();
+			queue.pop();
+
+			currentState = previousState;
+			for (auto i : buttons[currentState.nextButton])
+			{
+				currentState.lights.flip(i);
+			}
+			presses = seen[previousState] + 1;
+
+			for (auto i = 0; i < buttons.size(); ++i)
+			{
+				auto nextState = currentState;
+				nextState.nextButton = i;
+				if (seen.contains(nextState))
+				{
+					continue;
+				}
+
+				seen[nextState] = presses;
+				queue.push(std::move(nextState));
+			}
+		}
+
+		return presses;
+	}
+
 	std::string Solve(const std::vector<std::string>& inputLines)
 	{
-		return "Puzzle10A not yet solved!";
+		auto totalPresses = 0ull;
+		for (const auto& line : inputLines)
+		{
+			auto desiredLights = std::bitset<10>{};
+			auto i = 0ull;
+			for (i = 1; line[i] != ']'; ++i)
+			{
+				if (line[i] == '#')
+				{
+					desiredLights.set(i - 1);
+				}
+			}
+
+			auto buttons = std::vector<std::vector<int>>{};
+			i += 2;
+			while (line[i] != '{')
+			{
+				auto j = i + 1;
+				while (line[j] != ')')
+				{
+					++j;
+				}
+
+				auto buttonLights = Utilities::ExtractInt32s(std::string_view(line.data() + i, j - i));
+				buttons.emplace_back(std::move(buttonLights));
+				i = j + 2;
+			}
+
+			auto presses = SolveMachine(desiredLights, buttons);
+			totalPresses += presses;
+		}
+
+		return std::to_string(totalPresses);
 	}
 
 	std::string Visualize(const std::vector<std::string>& inputLines)
--- AdventOfCode/AdventOfCode/Puzzle10B.cpp
+++ AdventOfCode/AdventOfCode/Puzzle10B.cpp
@@ -10,9 +10,134 @@ using namespace nu::console::vt;
 
 namespace Puzzle10B
 {
+	struct State
+	{
+		std::array<int, 10> joltageLevels;
+		size_t nextButton = 0;
+
+		auto operator<=>(const State&) const = default;
+	};
+} // namespace Puzzle10B
+
+IMPLEMENT_STD_HASH(
+	Puzzle10B::State,
+	value.joltageLevels[0],
+	value.joltageLevels[1],
+	value.joltageLevels[2],
+	value.joltageLevels[3],
+	value.joltageLevels[4],
+	value.joltageLevels[5],
+	value.joltageLevels[6],
+	value.joltageLevels[7],
+	value.joltageLevels[8],
+	value.joltageLevels[9],
+	value.nextButton);
+
+namespace Puzzle10B
+{
+	uint64_t SolveMachine(const std::array<int, 10>& desiredJoltageLevels, const std::vector<std::vector<int>>& buttons)
+	{
+		 auto queue = std::queue<State>{};
+		 auto seen = std::unordered_map<State, uint64_t>{};
+		 for (auto i = 0; i < buttons.size(); ++i)
+		{
+			auto nextState = State{};
+			nextState.nextButton = i;
+			seen[nextState] = 0;
+			queue.push(std::move(nextState));
+		 }
+
+		 auto currentState = State{};
+		 auto presses = 0ull;
+		 while (currentState.joltageLevels != desiredJoltageLevels)
+		{
+			auto previousState = queue.front();
+			queue.pop();
+
+			currentState = previousState;
+			for (auto i : buttons[currentState.nextButton])
+			{
+				currentState.joltageLevels[i]++;
+			}
+			presses = seen[previousState] + 1;
+
+			for (auto i = 0; i < buttons.size(); ++i)
+			{
+				auto nextState = currentState;
+				nextState.nextButton = i;
+				if (seen.contains(nextState))
+				{
+					continue;
+				}
+
+				bool impossible = false;
+				for (auto k = 0; k < desiredJoltageLevels.size(); ++k)
+				{
+					if (currentState.joltageLevels[k] > desiredJoltageLevels[k])
+					{
+						impossible = true;
+						break;
+					}
+				}
+
+				if (impossible)
+				{
+					continue;
+				}
+
+				seen[nextState] = presses;
+				queue.push(std::move(nextState));
+			}
+		}
+
+		return presses;
+	}
+
+	// Does not terminate on full input. Did not optimize. Solved with Python Z3.
 	std::string Solve(const std::vector<std::string>& inputLines)
 	{
-		return "Puzzle10B not yet solved!";
+		if (inputLines.size() > 3)
+		{
+			return "Solved in Python with z3. :( :( :(";
+		}
+
+		auto totalPresses = 0ull;
+		for (const auto& line : inputLines)
+		{
+			auto i = 0ull;
+			for (i = 1; line[i] != ']'; ++i)
+			{
+			}
+
+			auto buttons = std::vector<std::vector<int>>{};
+			i += 2;
+			while (line[i] != '{')
+			{
+				auto j = i + 1;
+				while (line[j] != ')')
+				{
+					++j;
+				}
+
+				auto buttonLights = Utilities::ExtractInt32s(std::string_view(line.data() + i, j - i));
+				buttons.emplace_back(std::move(buttonLights));
+				i = j + 2;
+			}
+
+			auto desiredJoltageLevels = std::array<int, 10>{};
+			auto counters = ExtractInt32s(std::string_view(line.data() + i, line.size() - i));
+			auto k = 0ull;
+			for (auto counter : counters)
+			{
+				desiredJoltageLevels[k++] = counter;
+			}
+
+			auto presses = SolveMachine(desiredJoltageLevels, buttons);
+			std::cout << presses << '\t' << line << '\n';
+			totalPresses += presses;
+		}
+
+		return std::to_string(totalPresses);
 	}
 
 	std::string Visualize(const std::vector<std::string>& inputLines)
