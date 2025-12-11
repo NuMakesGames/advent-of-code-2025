5d757b26622c7468abe8514a44000112b6b2fbd1
Marcus Reid <NuMakesGames@users.noreply.github.com>
2025-12-11 05:22:20 +0000

    Solve day 11.

--- AdventOfCode/AdventOfCode/Puzzle11A.cpp
+++ AdventOfCode/AdventOfCode/Puzzle11A.cpp
@@ -10,9 +10,34 @@ using namespace nu::console::vt;
 
 namespace Puzzle11A
 {
+	// 662
 	std::string Solve(const std::vector<std::string>& inputLines)
 	{
-		return "Puzzle11A not yet solved!";
+		auto nodes = std::unordered_map<std::string, std::vector<std::string>>{};
+		for (const auto& line : inputLines)
+		{
+			auto tokens = Utilities::SplitString(line, ": ");
+			nodes[tokens[0]] = Utilities::SplitString(tokens[1], " ");
+		}
+
+		auto paths = 0ull;
+		auto queue = std::queue<std::string>{};
+		queue.push_range(nodes["you"]);
+		while (!queue.empty())
+		{
+			auto node = queue.front();
+			queue.pop();
+
+			if (node == "out")
+			{
+				++paths;
+				continue;
+			}
+
+			queue.push_range(nodes[node]);
+		}
+
+		return std::to_string(paths);
 	}
 
 	std::string Visualize(const std::vector<std::string>& inputLines)
--- AdventOfCode/AdventOfCode/Puzzle11B.cpp
+++ AdventOfCode/AdventOfCode/Puzzle11B.cpp
@@ -10,10 +10,76 @@ using namespace nu::console::vt;
 
 namespace Puzzle11B
 {
+	using Path = std::vector<std::string>;
+	using Paths = std::vector<Path>;
+	using Graph = std::unordered_map<std::string, std::vector<std::string>>;
+	using Memo = std::unordered_map<std::string, Paths>;
+
+	Paths DepthFirstSearchAllPaths(const std::string& current, const std::string& target, const Graph& graph, std::unordered_map<std::string, Paths>& memo)
+	{
+		if (memo.contains(current))
+		{
+			return memo[current];
+		}
+
+		auto result = Paths{};
+		if (current == target)
+		{
+			auto path = Path{};
+			path.push_back(target);
+			result.push_back(path);
+			memo[current] = result;
+			return result;
+		}
+
+		auto itNext = graph.find(current);
+		if (itNext != graph.end())
+		{
+			for (const auto& next : itNext->second)
+			{
+				auto paths = DepthFirstSearchAllPaths(next, target, graph, memo);
+				for (const auto& path : paths)
+				{
+					auto newPath = Path{};
+					newPath.push_back(current);
+					newPath.insert_range(newPath.end(), path);
+					result.push_back(newPath);
+				}
+			}
+		}
+
+		memo[current] = result;
+		return result;
+	}
+
+
+	// 114672848 too low
 	std::string Solve(const std::vector<std::string>& inputLines)
 	{
-		return "Puzzle11B not yet solved!";
+		auto nodes = std::unordered_set<std::string>{};
+		auto graph = Graph{};
+		for (const auto& line : inputLines)
+		{
+			auto tokens = Utilities::SplitString(line, ": ");
+			auto destinations = Utilities::SplitString(tokens[1], " ");
+			graph[tokens[0]] = destinations;
+			nodes.insert(tokens[0]);
+			nodes.insert_range(destinations);
+		}
+
+		// Wrote out graphviz and visualized to confirm DAG
+		auto svrToFftMemo = Memo{};
+		auto svrToFftPaths = DepthFirstSearchAllPaths("svr", "fft", graph, svrToFftMemo);
+
+		auto fftToDacMemo = Memo{};
+		auto fftToDacPaths = DepthFirstSearchAllPaths("fft", "dac", graph, fftToDacMemo);
+
+		auto dacToOutMemo = Memo{};
+		auto dacToOutPaths = DepthFirstSearchAllPaths("dac", "out", graph, dacToOutMemo);
+
+		return std::to_string(svrToFftPaths.size() * fftToDacPaths.size() * dacToOutPaths.size());
 	}
+
 
 	std::string Visualize(const std::vector<std::string>& inputLines)
 	{
